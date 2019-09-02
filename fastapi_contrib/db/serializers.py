65bfc46270ecbd4bf10d4afdd994377753a6a7b3
Lev Rubel <l@datacorp.ee>
2019-09-02 19:17:54 +0300

    this commit fixes #6: you can now safely create childs of modelserializer and all your attrs and functions will be available

--- fastapi_contrib/db/serializers.py
+++ fastapi_contrib/db/serializers.py
@@ -1,6 +1,7 @@
+from abc import ABC
 from typing import Iterable, List
 
-from pydantic import BaseModel, create_model
+from pydantic import BaseModel, create_model, Required
 
 
 class Serializer(BaseModel):
@@ -19,7 +20,7 @@ class Serializer(BaseModel):
         self.id = instance.id
         return instance
 
-    async def update_one(self, filter_kwargs: dict = {}):
+    async def update_one(self, filter_kwargs):
         instance = self.Meta.model(**self.__values__)
         await instance.update_one(filter_kwargs=filter_kwargs, **self.dict())
         return instance
@@ -42,13 +43,41 @@ class Serializer(BaseModel):
         return original
 
 
+class AbstractMeta(ABC):
+    exclude = ()
+    model = None
+
+
 class ModelSerializer(Serializer):
 
     def __new__(cls, *args, **kwargs):
         _fields = {}
-        for f, t in cls.Meta.model.__fields__.items():
-            if f not in cls.Meta.exclude:
-                _fields.update({f: (t.type_, t.default)})
+
+        _Meta = getattr(cls, "Meta", type("Meta"))
+        Meta = type("Meta", (_Meta, AbstractMeta), {})
+
+        if hasattr(Meta, "model") and Meta.model is not None:
+            for f, t in Meta.model.__fields__.items():
+                if f not in Meta.exclude:
+                    f_def = t.default
+                    if t.required:
+                        f_def = Required
+                    _fields.update({f: (t.type_, f_def)})
+
+        for f, t in cls.__fields__.items():
+            if f not in Meta.exclude:
+                f_def = t.default
+                if t.required:
+                    f_def = Required
+                _fields.update({f: (t.type_, f_def)})
+
         new_model = create_model(cls.__name__, __base__=Serializer, **_fields)
-        new_model.Meta = cls.Meta
+
+        reserved_attrs = ["Meta"]
+        for attr, value in cls.__dict__.items():
+            if not attr.startswith("_") and attr not in reserved_attrs:
+                setattr(new_model, attr, value)
+
+        setattr(new_model, "Meta", Meta)
+
         return new_model(*args, **kwargs)
--- tests/db/test_serializers.py
+++ tests/db/test_serializers.py
@@ -1,2 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pytest
+from pydantic import BaseModel, ValidationError
+
+from fastapi_contrib.db.serializers import Serializer, ModelSerializer
+
+
+def test_serializer_inheritance_works():
+    class TestSerializer(Serializer):
+        a = 1
+
+        def b(self):
+            return "b"
+
+    serializer = TestSerializer()
+    assert serializer.a == 1
+    assert serializer.b() == "b"
+
+
+def test_model_serializer_inheritance_works():
+    class Model(BaseModel):
+        ...
+
+    class TestSerializer(ModelSerializer):
+        a = 1
+        c: str
+        d: int = None
+
+        def b(self):
+            return "b"
+
+        class Meta:
+            model = Model
+
+    serializer = TestSerializer(c="2", d=3)
+    assert serializer.a == 1
+    assert serializer.c == "2"
+    assert serializer.d == 3
+    assert serializer.b() == "b"
+
+    with pytest.raises(ValidationError):
+        TestSerializer(c=dict(), d="asd")
+
+    with pytest.raises(ValidationError):
+        TestSerializer(c=None, d=None)
+
+    with pytest.raises(ValidationError):
+        TestSerializer()
