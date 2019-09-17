f10faa0931e6f3536a100654f07d1c23d9b169a5
Lev Rubel <l@datacorp.ee>
2019-09-17 16:01:30 +0300

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/identixone/fastapi_contrib",
-    version="0.1.1",
+    version="0.1.2",
     zip_safe=False,
 )
