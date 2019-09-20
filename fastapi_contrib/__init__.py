12b5b046fdbaf86950dcde49aaff8954e29607f4
Lev Rubel <l@datacorp.ee>
2019-09-20 16:41:05 +0300

    Bump version: 0.1.3 → 0.1.4

--- fastapi_contrib/__init__.py
+++ fastapi_contrib/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Lev Rubel"""
 __email__ = 'l@datacorp.ee'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/identixone/fastapi_contrib",
-    version="0.1.3",
+    version="0.1.4",
     zip_safe=False,
 )
